e58528ee619fe9b2484ec4ef6f4a1f6da0c3bfb0
nasrin <n.mosh4yedi@gmail.com>
2019-08-30 14:22:53 +0430

    my work

--- src/components/MyWork/MyWork.js
+++ src/components/MyWork/MyWork.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import Img from "react-image";
+
 import { useStyles } from "./MyWorkStyle";
-import { Button } from "@material-ui/core";
+import Work1 from "./../../assets/images/instance/2.jpg";
+import Work2 from "./../../assets/images/instance/549eb36bf8ed7831773ec3cb3641dfe3.png";
+import Work3 from "./../../assets/images/instance/5-web-design-trends-in-2019.jpg";
+import Work4 from "./../../assets/images/instance/dribbble_small_2x.png";
+import Work5 from "./../../assets/images/instance/e4ab7241f612de18d8951e659c756c99.png";
+import Work6 from "./../../assets/images/instance/hirefullstackdeveloperindia_marketing_post_01_12_04_2019_900x675.jpg";
 
 export default function Team() {
   const classes = useStyles();
@@ -21,6 +28,28 @@ export default function Team() {
           <Typography variant="h3" align="center">
             WHAT WE DO
           </Typography>
+        </Grid>
+        <Grid item lg={10}>
+          <Grid container justify="space-around" alignItems="center">
+            <Grid item lg={3} style={{ margin: "20px" }}>
+              <Img src={Work1} style={{ display: "block", width: "100%" }} />
+            </Grid>
+            <Grid item lg={3} style={{ margin: "20px" }}>
+              <Img src={Work2} style={{ display: "block", width: "100%" }} />
+            </Grid>
+            <Grid item lg={3} style={{ margin: "20px" }}>
+              <Img src={Work3} style={{ display: "block", width: "100%" }} />
+            </Grid>
+            <Grid item lg={3} style={{ margin: "20px" }}>
+              <Img src={Work4} style={{ display: "block", width: "100%" }} />
+            </Grid>
+            <Grid item lg={3} style={{ margin: "20px" }}>
+              <Img src={Work5} style={{ display: "block", width: "100%" }} />
+            </Grid>
+            <Grid item lg={3} style={{ margin: "20px" }}>
+              <Img src={Work6} style={{ display: "block", width: "100%" }} />
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>
